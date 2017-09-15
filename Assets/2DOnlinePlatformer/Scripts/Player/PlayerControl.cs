d79954b278c764fcf828f27ef7cc69e672277094
Michael J. Calkins <michaeljamescalkins@gmail.com>
2017-09-15 12:11:54 -0700

    Fixes to player controls

--- Assets/2DOnlinePlatformer/Scripts/Player/PlayerControl.cs
+++ Assets/2DOnlinePlatformer/Scripts/Player/PlayerControl.cs
@@ -6,7 +6,8 @@ to Update, FixedUpdate, Start the other players do not come because they disable
 (because they are not the owner of this prefab "hero" (which has this script - PlayerControl) which was created in OnJoinedRoom(script ConnectRoom))
 
 Network Description */
-public class PlayerControl : MonoBehaviour {
+public class PlayerControl : MonoBehaviour
+{
     // For determining which way the player is currently facing.
     [HideInInspector]
     public bool facingRight = true;
@@ -50,33 +51,36 @@ public class PlayerControl : MonoBehaviour {
     // Reference to the player's animator component.
     private Animator anim;
 
-    void Start () {
+    void Start ()
+    {
         // Setting up references.
         groundCheck = transform.Find ("groundCheck");
         anim = GetComponent<Animator> ();
     }
 
-    void Update () {
+    void Update ()
+    {
         // The player is grounded if a linecast to the groundcheck position hits anything on the ground layer.
         grounded = Physics2D.Linecast (transform.position, groundCheck.position, 1 << LayerMask.NameToLayer ("Ground"));
 
         // If the jump button is pressed and the player is grounded then the player should jump.
-        if (Input.GetButtonDown ("Jump") && grounded) {
+        if (Input.GetKeyDown (KeyCode.W) && grounded) {
             jump = true;
         }
 
-        if (Input.GetButtonDown ("Down")) {
+        if (Input.GetKeyDown (KeyCode.S)) {
             sit = true;
         }
 
-        if (Input.GetButtonUp ("Down")) {
+        if (Input.GetKeyUp (KeyCode.S)) {
             sit = false;
         }
 
         flying = Input.GetMouseButton (1);
     }
 
-    void FixedUpdate () {
+    void FixedUpdate ()
+    {
         float h = 0;
         AnimatorStateInfo stateInfo = anim.GetCurrentAnimatorStateInfo (0);
         if (!stateInfo.IsName ("Base Layer.Sit")) {
@@ -94,59 +98,87 @@ public class PlayerControl : MonoBehaviour {
         }
 
         // If the player should fly...
-        if (flying && Mathf.Abs (GetComponent<Rigidbody2D> ().velocity.y) < maxSpeedY) {
+        if (flying) {
             // Set the Jump animator trigger parameter.
             anim.SetTrigger ("Jump");
 
             // Play a random jump audio clip.
             int i = Random.Range (0, jumpClips.Length);
-            AudioSource.PlayClipAtPoint (jumpClips[i], transform.position);
+            AudioSource.PlayClipAtPoint (jumpClips [i], transform.position);
 
             // Add a vertical force to the player.
             GetComponent<Rigidbody2D> ().AddForce (new Vector2 (0f, flyingForce));
+        }
+
+        // If the player should jump...
+        if (jump) {
+            // Set the Jump animator trigger parameter.
+            anim.SetTrigger ("Jump");
+
+            // Play a random jump audio clip.
+            int i = Random.Range (0, jumpClips.Length);
+            AudioSource.PlayClipAtPoint (jumpClips [i], transform.position);
+
+            // Add a vertical force to the player.
+            GetComponent<Rigidbody2D> ().AddForce (new Vector2 (0f, jumpForce));
+
+            // Make sure the player can't jump again until the jump conditions from Update are satisfied.
+            jump = false;
+        }
+
+        // Detect what side of the player the mouse is on and flip according to that.
+        Vector2 mouse = new Vector2 (Input.mousePosition.x, Input.mousePosition.y);
+        Vector3 playerScreenPoint = Camera.main.WorldToScreenPoint (GetComponent<Rigidbody2D> ().transform.position);
+        if (mouse.x < playerScreenPoint.x) {
+            FlipLeft ();
+        } else {
+            FlipRight ();
+        }
+
+        // If the player's vertical velocity is greater than the maxSpeedY...
+        if (Mathf.Abs (GetComponent<Rigidbody2D> ().velocity.y) > maxSpeedY) {
+            // ... set the player's velocity to the maxSpeedY in the y axis.
+            GetComponent<Rigidbody2D> ().velocity = new Vector2 (GetComponent<Rigidbody2D> ().velocity.x, Mathf.Sign (GetComponent<Rigidbody2D> ().velocity.y) * maxSpeedY);
         }
 
         // If the player's horizontal velocity is greater than the maxSpeedX...
         if (Mathf.Abs (GetComponent<Rigidbody2D> ().velocity.x) > maxSpeedX) {
             // ... set the player's velocity to the maxSpeedX in the x axis.
             GetComponent<Rigidbody2D> ().velocity = new Vector2 (Mathf.Sign (GetComponent<Rigidbody2D> ().velocity.x) * maxSpeedX, GetComponent<Rigidbody2D> ().velocity.y);
-
-            // If the input is moving the player right and the player is facing left...
-            if (h > 0 && !facingRight) {
-                // ... flip the player.
-                Flip ();
-            } else if (h < 0 && facingRight) {
-                // Otherwise if the input is moving the player left and the player is facing right...flip the player.
-                Flip ();
-            }
-
-            // If the player should jump...
-            if (jump) {
-                // Set the Jump animator trigger parameter.
-                anim.SetTrigger ("Jump");
-
-                // Play a random jump audio clip.
-                int i = Random.Range (0, jumpClips.Length);
-                AudioSource.PlayClipAtPoint (jumpClips[i], transform.position);
-
-                // Add a vertical force to the player.
-                GetComponent<Rigidbody2D> ().AddForce (new Vector2 (0f, jumpForce));
-
-                // Make sure the player can't jump again until the jump conditions from Update are satisfied.
-                jump = false;
-            }
-
-            anim.SetBool ("Sit", sit);
         }
+
+        //        anim.SetBool ("Sit", sit);
     }
 
-    void Flip () {
-        // Switch the way the player is labelled as facing.
-        facingRight = !facingRight;
+    void FlipRight ()
+    {
+        // Only flip the player once or you'll end up with infinite flipping
+        if (facingRight)
+            return;
+            
+        Flip ();
 
+        // Player is now facing right
+        facingRight = true;
+    }
+
+    void FlipLeft ()
+    {
+        // Only flip the player once or you'll end up with infinite flipping
+        if (!facingRight)
+            return;
+
+        Flip ();
+            
+        // Player is now facing left
+        facingRight = false;
+    }
+
+    void Flip ()
+    {
         // Multiply the player's x local scale by -1.
         Vector3 theScale = transform.localScale;
         theScale.x *= -1;
-        transform.localScale = theScale;
+        transform.localScale = theScale;   
     }
 }
