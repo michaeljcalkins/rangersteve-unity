b06d8c33dfc212fcaa150aee526a23e4266a8093
Michael J. Calkins <michaeljamescalkins@gmail.com>
2017-09-15 17:12:37 -0700

    WIP

--- Assets/2DOnlinePlatformer/Scripts/Weapon/Ammo.cs
+++ Assets/2DOnlinePlatformer/Scripts/Weapon/Ammo.cs
@@ -1,71 +1,80 @@
-﻿using UnityEngine;
+﻿/**
+ * unlike a bomb is removed after any first collision
+ *
+ * !!!! We sometimes go into the collider twice(OnCollisionEnter2D,OnTriggerEnter2D) - bag Physics Unity !!!! This is a very important point in PUN
+ * Because of this we can send network data several times instead of once.
+ * Extra traffic costs. And in the PUN delay I attack quickly.
+ * Each extra byte of the transmitted traffic is already a problem.
+ */
+
+using UnityEngine;
 using System.Collections;
-/* Network Description
 
-unlike a bomb is removed after any first collision
-
-!!!! We sometimes go into the collider twice(OnCollisionEnter2D,OnTriggerEnter2D) - bag Physics Unity !!!! This is a very important point in PUN
-Because of this we can send network data several times instead of once.
-Extra traffic costs. And in the PUN delay I attack quickly.
-Each extra byte of the transmitted traffic is already a problem.
-
-Network Description */
 public class Ammo : Photon.MonoBehaviour
 {
-    public float ammo_speed;
+    public float ammoSpeed = 2000;
 
     // Prefab of explosion effect.
-	public GameObject explosion;
+    public GameObject explosion;
 
     // We sometimes go into the collider twice(OnCollisionEnter2D,OnTriggerEnter2D) - bag Physics Unity. It is necessary once
     bool flag;
 
-    void Awake()
+    Vector3 mouse_pos;
+    Transform target;
+
+    //Assign to the object you want to rotate
+    Vector3 object_pos;
+    float angle;
+
+    void Awake ()
     {
-        GetComponent<Rigidbody2D>().velocity = new Vector2(transform.right.x* ammo_speed, 0); // shooting in the direction of movement
+        // Add force in the direction described
+        GetComponent<Rigidbody2D> ().AddForce (transform.up * ammoSpeed);
+    }
+
+    void Update ()
+    {  
+
     }
 
     void OnTriggerEnter2D (Collider2D col)
-	{
-        if (flag || !photonView.isMine || col.gameObject.tag == "Player" && col.transform.GetComponent<PhotonView>().isMine) return;
+    {
+        if (flag || !photonView.isMine || col.gameObject.tag == "Player" && col.transform.GetComponent<PhotonView> ().isMine)
+            return;
         flag = true;
 
-        if (col.tag == "WeaponBox")
-        {
-            col.gameObject.GetComponent<PhotonView>().RPC("Explode", PhotonTargets.All, col.transform.position);
+        if (col.tag == "WeaponBox") {
+            col.gameObject.GetComponent<PhotonView> ().RPC ("Explode", PhotonTargets.All, col.transform.position);
         }
 
         // Otherwise if the player manages to shoot himself...
-        else if (col.gameObject.tag != "Player")
-        {
+        else if (col.gameObject.tag != "Player") {
             if (explosion != null) //Some ammunition does not leave explosions after the collision // They have an explosion = null
-                PhotonNetwork.Instantiate(explosion.name, transform.position, Quaternion.Euler(0f, 0f, Random.Range(0f, 360f)), 0);
-        }
-        else if (col.gameObject.tag == "Player" && !col.transform.GetComponent<PhotonView>().isMine)
-        {
-            int i = Random.Range(0, col.GetComponent<PlayerDeath>().ouchClips.Length);
-            col.gameObject.GetComponent<PhotonView>().RPC("Death", PhotonTargets.All, i);
+                PhotonNetwork.Instantiate (explosion.name, transform.position, Quaternion.Euler (0f, 0f, Random.Range (0f, 360f)), 0);
+        } else if (col.gameObject.tag == "Player" && !col.transform.GetComponent<PhotonView> ().isMine) {
+            int i = Random.Range (0, col.GetComponent<PlayerDeath> ().ouchClips.Length);
+            col.gameObject.GetComponent<PhotonView> ().RPC ("Death", PhotonTargets.All, i);
 
-            if(explosion!=null) // Some ammunition does not leave explosions after the collision // They have an explosion = null
-                PhotonNetwork.Instantiate(explosion.name, transform.position, Quaternion.Euler(0f, 0f, Random.Range(0f, 360f)), 0);
+            if (explosion != null) // Some ammunition does not leave explosions after the collision // They have an explosion = null
+                PhotonNetwork.Instantiate (explosion.name, transform.position, Quaternion.Euler (0f, 0f, Random.Range (0f, 360f)), 0);
         }
 
-        StartCoroutine(bullet_self_destruct_());
+        StartCoroutine (bullet_self_destruct_ ());
     }
 
-    IEnumerator bullet_self_destruct_() // Without this there will be problems playing sound of a shot when the ammo is destroyed immediately after the spawn
+    IEnumerator bullet_self_destruct_ () // Without this there will be problems playing sound of a shot when the ammo is destroyed immediately after the spawn
     {
-        GetComponent<BoxCollider2D>().enabled = false;
+        GetComponent<BoxCollider2D> ().enabled = false;
 
-        transform.GetChild(0).GetComponent<SpriteRenderer>().enabled = false;
-        if (transform.childCount > 1) // for rocket
-        {
-            transform.GetChild(1).GetComponent<SpriteRenderer>().enabled = false;
-            transform.GetChild(2).gameObject.SetActive(false);
+        transform.GetChild (0).GetComponent<SpriteRenderer> ().enabled = false;
+        if (transform.childCount > 1) { // for rocket
+            transform.GetChild (1).GetComponent<SpriteRenderer> ().enabled = false;
+            transform.GetChild (2).gameObject.SetActive (false);
         }
-        while (GetComponent<AudioSource>().isPlaying == true)
-            yield return new WaitForSeconds(0.1f);
+        while (GetComponent<AudioSource> ().isPlaying == true)
+            yield return new WaitForSeconds (0.1f);
 
-        PhotonNetwork.Destroy(gameObject); // You can not go twice. There will be a red bug PUN - "Ev Destroy Failed."
+        PhotonNetwork.Destroy (gameObject); // You can not go twice. There will be a red bug PUN - "Ev Destroy Failed."
     }
 }
--- Assets/2DOnlinePlatformer/Scripts/Weapon/Weapons.cs
+++ Assets/2DOnlinePlatformer/Scripts/Weapon/Weapons.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+
 /* Description
 
 Move point position spawn ammunition weapons if the player crouches (Vector3 Spawn_point)
@@ -11,76 +12,107 @@ in prefab box weapon script is disabled in order that we could not use the behav
 
 Description */
 public class Weapons : MonoBehaviour
-{ // Use this for initialization Weapon
-    protected bool fire;                      // The player is currently shooting?
-    protected Animator anim;                  // Reference to the Animator component.
-    public Sprite picture_weapon;             // Sprite weapon
-    public GameObject ammunition;             // Ammunition weapon
-    public Vector3 spawn_point;               // Point  spawn ammunition weapons
-    public bool weapon_animation;             // Animation of recoil of the weapon after a shot from it  
-    public bool front;                        // Weapon in front of the player or behind the player's sprite?
-    protected Vector3 Spawn_point             // Point  spawn ammunition weapons
-    {
-        get
-        {
-            Vector3 spawn = transform.parent.position + new Vector3(spawn_point.x * Mathf.Sign(transform.parent.localScale.x), spawn_point.y, spawn_point.z);
-            AnimatorStateInfo stateInfo = anim.GetCurrentAnimatorStateInfo(0);
-            if (stateInfo.IsName("Base Layer.Sit"))
+{
+    // Use this for initialization Weapon
+    protected bool fire;
+
+    // The player is currently shooting?
+    protected Animator anim;
+
+    // Reference to the Animator component.
+    public Sprite picture_weapon;
+
+    // Sprite weapon
+    public GameObject ammunition;
+
+    // Ammunition weapon
+    public Vector3 spawn_point;
+
+    // Point  spawn ammunition weapons
+    public bool weapon_animation;
+
+    // Animation of recoil of the weapon after a shot from it
+    public bool front;
+
+    // Weapon in front of the player or behind the player's sprite?
+    // Point  spawn ammunition weapons
+    protected Vector3 Spawn_point {             
+        get {
+            Vector3 spawn = transform.parent.position + new Vector3 (spawn_point.x * Mathf.Sign (transform.parent.localScale.x), spawn_point.y, spawn_point.z);
+            AnimatorStateInfo stateInfo = anim.GetCurrentAnimatorStateInfo (0);
+            if (stateInfo.IsName ("Base Layer.Sit"))
                 return spawn -= Vector3.up * 0.6f;
             else
                 return spawn;
         }
     }
-    public int amount;                      // the number of weapons ammunition
-    protected int Amount                    // the number of weapons ammunition
-    {
-        get
-        {
+
+    // the number of weapons ammunition
+    public int amount;
+
+    protected int Amount {                    
+        get {
             return amount;
         }
-        set
-        {
+
+        set {
             amount = value;
-            if(amount==0)
-            {
-                GetComponent<SpriteRenderer>().sprite = null;
-                Destroy(GetComponents<Behaviour>()[GetComponents<Behaviour>().Length - 1]);
+            if (amount == 0) {
+                GetComponent<SpriteRenderer> ().sprite = null;
+                Destroy (GetComponents<Behaviour> () [GetComponents<Behaviour> ().Length - 1]);
             }
         }
     }
-    protected virtual void Update()
+
+    protected virtual void Update ()
     {
-        //In FixedUpdate() sometimes returns a Input.GetButtonDown("Fire1") ==  false at the time of pressing
-        if (Input.GetButtonDown("Fire1"))
-        {
+        // In FixedUpdate() sometimes returns a Input.GetButtonDown("Fire1") ==  false at the time of pressing
+        if (Input.GetMouseButtonDown (0)) {
             fire = true;
         }
     }
-    protected virtual void FixedUpdate()
-    {// This is all necessary in order to correctly transmit over the network " anim.SetTrigger("Shoot"); ". Example - script Bazooka .
-     // In the Update, sometimes the transfer of this SetTrigger over the network does not have time to transmit
-        if (fire)
-        {
+
+    protected virtual void FixedUpdate ()
+    {
+        // This is all necessary in order to correctly transmit over the 
+        // network " anim.SetTrigger("Shoot"); ". 
+        // Example - script Bazooka .
+
+        // In the Update, sometimes the transfer of this SetTrigger over 
+        // the network does not have time to transmit
+        if (fire) {
+            // Prevents double firing by accident
             fire = false;
             if (weapon_animation)
-                anim.SetTrigger("Shoot");
+                anim.SetTrigger ("Shoot");
+
+            Vector3 pos = Input.mousePosition;
+            pos.z = transform.position.z - Camera.main.transform.position.z;
+            pos = Camera.main.ScreenToWorldPoint (pos);
+
+            // Quaternion.LookRotation (new Vector3 (0, 0, Mathf.Sign (transform.root.localScale.x)))
+            var q = Quaternion.FromToRotation (Vector3.up, pos - transform.position);
+            
             // ... instantiate the prefab facing right or left and set it's velocity to the right or left. 
-            PhotonNetwork.Instantiate("Ammo/" + ammunition.name, Spawn_point, Quaternion.LookRotation(new Vector3(0, 0, Mathf.Sign(transform.root.localScale.x))), 0);
+            PhotonNetwork.Instantiate ("Ammo/" + ammunition.name, Spawn_point, q, 0);
 
             Amount--;
         }
     }
-    protected virtual void Start()
+
+    protected virtual void Start ()
     {
-        GetComponent<SpriteRenderer>().sortingOrder = front ? 1 : 0;
-        anim = transform.root.GetComponent<Animator>();
-        GetComponent<SpriteRenderer>().sprite = picture_weapon;
+        GetComponent<SpriteRenderer> ().sortingOrder = front ? 1 : 0;
+        anim = transform.root.GetComponent<Animator> ();
+        GetComponent<SpriteRenderer> ().sprite = picture_weapon;
     }
-    private void OnDisable()
+
+    private void OnDisable ()
     {
-            GetComponents<Behaviour>()[GetComponents<Behaviour>().Length - 1].enabled = true;
+        GetComponents<Behaviour> () [GetComponents<Behaviour> ().Length - 1].enabled = true;
     }
-    public void Initialization(Weapons new_, Weapons original)
+
+    public void Initialization (Weapons new_, Weapons original)
     {
         new_.picture_weapon = original.picture_weapon;
         new_.ammunition = original.ammunition;
