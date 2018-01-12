d3241272aabd72ea314779a7b0300289cdf0d8a6
Michael J. Calkins <michaeljamescalkins@gmail.com>
2018-01-12 08:42:47 -0800

    Add help panel and button

---
+++ Assets/Ranger Steve/Scripts/Level/HelpInfoController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelpInfoController : MonoBehaviour
+{
+
+    public bool isHelpInfoVisible = false;
+
+    public GameObject helpInfoPanel;
+
+    // Use this for initialization
+    void Start()
+    {
+        helpInfoPanel = GameObject.Find("HelpInfo");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        helpInfoPanel.gameObject.SetActive(isHelpInfoVisible);
+    }
+
+    public void HandleShowHelpInfo()
+    {
+        isHelpInfoVisible = true;
+    }
+
+    public void HandleHideHelpInfo()
+    {
+        isHelpInfoVisible = false;
+    }
+}
