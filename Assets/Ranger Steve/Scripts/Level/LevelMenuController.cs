2f734d549351c46d610e5e4c0cfbc2f75773f57a
Michael J. Calkins <michaeljamescalkins@gmail.com>
2018-01-12 13:18:25 -0800

    Add pre order to game

---
+++ Assets/Ranger Steve/Scripts/Level/LevelMenuController.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.Analytics;
+
+namespace Com.LavaEagle.RangerSteve
+{
+    public class LevelMenuController : MonoBehaviour
+    {
+        public void HandlePreorderClick()
+        {
+            Analytics.CustomEvent("Clicked Pre-order In Game");
+            Application.OpenURL("https://www.humblebundle.com/g/rangersteve");
+        }
+    }
+}
