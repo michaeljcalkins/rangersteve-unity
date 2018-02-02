8b98ff95f4c517444bdd58a75c7ac5baefae1668
Michael J. Calkins <michaeljamescalkins@gmail.com>
2018-02-02 14:23:51 -0800

    Updates

--- Assets/Ranger Steve/Scripts/Level/KillTrigger.cs
+++ Assets/Ranger Steve/Scripts/Level/KillTrigger.cs
@@ -1,18 +1,5 @@
 ﻿using UnityEngine;
-using UnityEngine.UI;
 
-/* Network Description
-
-If this player dies or if there is one alive player in the room,
-or if everyone dies (the last two players killed each other simultaneously),
-then all players again create prefab of the player player (Remover script). Nobody leaves the room
-
-!!!! We sometimes go into the collider twice(OnCollisionEnter2D,OnTriggerEnter2D) - bag Physics Unity !!!! This is a very important point in PUN
-Because of this we can send network data several times instead of once.
-Extra traffic costs. And in the PUN delay I attack quickly.
-Each extra byte of the transmitted traffic is already a problem.
-
-Network Description */
 namespace Com.LavaEagle.RangerSteve
 {
     public class KillTrigger : Photon.MonoBehaviour
@@ -22,11 +9,8 @@ namespace Com.LavaEagle.RangerSteve
 
         void OnTriggerEnter2D(Collider2D other)
         {
-            if (other.GetComponent<PhotonView>() && !other.GetComponent<PhotonView>().isMine)
-                return;
-
             // Play kill trigger splash animation here
-            PhotonNetwork.Instantiate(splash.name, other.transform.position, transform.rotation, 0);
+            Instantiate(Resources.Load("splash"), other.transform.position, transform.rotation);
 
             if (other.tag == "Local Player")
             {
